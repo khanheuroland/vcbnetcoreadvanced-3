f6ba4dba73ce7cd8113a12f205e04476ab28de73
Khanh Tran <khanh.euroland@gmail.com>
2024-09-21 08:28:24 +0700

    Add settings

--- Program.cs
+++ Program.cs
@@ -9,13 +9,21 @@ using Serilog.Settings.Configuration;
 var builder = WebApplication.CreateBuilder(args);
 
 //builder.Logging.AddLog4Net("log4net.config"); //Add Log4Net provider
-
+/*
 var configuration = new LoggerConfiguration();
+configuration.Enrich.FromLogContext()
+    .Enrich.WithProperty("ProjectName", ".NET Core VCB");
+    
 configuration.WriteTo.File(new JsonFormatter(), "D:\\Log\\jsonLogFile.json");
-configuration.WriteTo.Seq("http://localhost:5341", Serilog.Events.LogEventLevel.Warning);
+configuration.WriteTo.Seq("http://localhost:5341", Serilog.Events.LogEventLevel.Information);
 Log.Logger = configuration.CreateLogger();
 builder.Services.AddLogging(logBuilder=>logBuilder.AddSerilog());
+*/
+builder.Host.UseSerilog((context, services, configuration)=>{
+    configuration.ReadFrom.Configuration(context.Configuration) //Read from setting file
+                 .ReadFrom.Services(services); //Read service of system
 
+});
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
